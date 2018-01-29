7d1f4a004b9d405b5d98b05d4306b860f6738a32
Josué Morales <jos_15_12_josue@hotmail.com>
2018-01-28 20:35:56 -0700

    Brute force attack functionality added
    not working correctly tho

--- main.js
+++ main.js
@@ -38,4 +38,64 @@ window.onload = function() {
     String.prototype.replaceAt = function(index, replacement) {
         return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
     }
+    
+    function isCorrect(diff, input, output){
+        for(var i = 0; i<input.length; i++){
+            var i_char = input.charCodeAt(i);
+            var o_char = output.charCodeAt(i);
+            if((o_char-i_char)!=diff){
+                return false;
+            }
+        }
+       return true;
+    }
+    
+    function getWord(diff, input){
+        var res = "";
+        for(var i = 0; i<input.length; i++){
+            var i_char = input.charCodeAt(i);
+            //res = res + String.fromCharCode(i_char+((26-diff)%26));
+            res = res + String.fromCharCode(i_char+diff);
+        }
+        return res;
+    }
+    
+    $( "#button_bfattack" ).click(function() {
+        var input = $("#input-text").val().toUpperCase();
+        input = input.replace(/ /g, "");
+        var output = $("#output-text").val().toUpperCase();
+        output = output.replace(/ /g, "");
+        
+        if(input.length == 0 || output.length == 0){
+            alert("An input is missing or both.");    
+            return;
+        }
+        if(input.length!=output.length){
+            alert("Both inputs must be of equal length.");    
+            return;
+        }
+        
+        var start = input.charCodeAt(0);
+        var end = output.charCodeAt(0);
+        var diff = end - start;
+        
+        $("#bfattack_results tbody").empty();
+        if(isCorrect(diff, input, output)){
+            $("#key_found").val("Key is: "+((26-diff)%26));//((26-diff)%26) o diff
+        }
+        else{
+            $("#key_found").val("No key found. Testing all possible shifts.");
+            for(var a=1; a<26; a++){
+                    $("#bfattack_results tbody").append("" +
+                        "<tr>"+
+                            "<td> "+a+" </td>" +
+                            "<td>"+getWord(a, input)+"</td> " +
+                        "</tr>");    
+            }
+        }
+            
+        
+        
+        
+    });
 }
